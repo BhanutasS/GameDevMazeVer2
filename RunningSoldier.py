995a57c3b8edaaf80ab69d67ed8384c0186619bc
bsavanachai <bhanutas.savanachai@agoda.com>
2023-11-20 19:55:35 +0700

    add level

--- RunningSoldier.py
+++ RunningSoldier.py
@@ -67,11 +67,7 @@ BULLET = pygame.image.load(os.path.join("assets/Other", "bullet_image.png"))
 
 BOMB = pygame.image.load(os.path.join("assets/Other","Bomb_03.png"))
 
-SLOWMO = pygame.image.load(os.path.join("assets/Other","Hero_Speed_Debuff.png"))
-
 scaled_image = pygame.transform.scale(BOMB, (64, 64))
-
-scaled_image_sm = pygame.transform.scale(SLOWMO, (64, 64))
 
 scaled_bg = pygame.transform.scale(BG, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -103,7 +99,6 @@ fireballs=[]
 bullet_items = []
 fireball_count = 3
 bomb_items=[]
-slowmo_items=[]
 back_ground = Background()
 
 class Fireball:
@@ -142,27 +137,6 @@ class Bomb:
     def draw(self, screen):
         scaled_image = pygame.transform.scale(BOMB, (64, 64))
         screen.blit(scaled_image, self.rect)
-    
-    def collides_with(self, obstacle):
-        # Use the rect attribute for collision detection
-        return self.rect.colliderect(obstacle.rect)
-
-class SlowMo:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.image = scaled_image_sm
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.speed = 20
-
-    def move(self):
-        self.rect.x += self.speed
-
-    def draw(self, screen):
-        scaled_image_sm = pygame.transform.scale(SLOWMO, (64, 64))
-        screen.blit(scaled_image_sm, self.rect)
     
     def collides_with(self, obstacle):
         # Use the rect attribute for collision detection
@@ -402,40 +376,18 @@ class BombItem:
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
-class SlowMoItem:
-    def __init__(self):
-        self.image = scaled_image_sm  # Replace with your item image path
-        self.rect = self.image.get_rect()
-        self.rect.x = SCREEN_WIDTH
-        self.rect.y = random.randint(100, 350)  # Random height
-        
-        self.music_channel = mixer.Channel(0)
-        self.music_channel.set_volume(0.2)
-
-        self.sounds_list = {
-            'slow': mixer.Sound('sounds/slow.wav'),
-        }
-
-    def update(self):
-        self.rect.x -= game_speed
-        if self.rect.x < -self.rect.width:
-            # Remove the item if it goes off the screen
-            slowmo_items.remove(self)
-
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
-
-def main():
-    global fireballs, fireball_count, death_count
-    global game_speed, x_pos_bg, y_pos_bg, points, obstacles
+def main_l1():
+    global fireballs, fireball_count, death_count, distance_l1
+    global game_speed, x_pos_bg, y_pos_bg, points_l1, obstacles
+    distance_l1 = 0
     run = True
     clock = pygame.time.Clock()
     player = soldier()
     cloud = Cloud()
-    game_speed = 10
+    game_speed = 2
     x_pos_bg = 0
     y_pos_bg = 380
-    points = 0
+    points_l1 = 0
     font = pygame.font.Font("freesansbold.ttf", 20)
     obstacles = []
     death_count = 0
@@ -450,17 +402,18 @@ def main():
         text_rect = text.get_rect(topleft=(10, 10))  # Position at top-left corner
         SCREEN.blit(text, text_rect)
 
-    def score():
-        global points, game_speed
-        points += 1
-        if points % 100 == 0:
-            game_speed += 2
-        with open("score.txt", "r") as f:
-            score_ints = [int(x) for x in f.read().split()]  
-            highscore = max(score_ints)
-            if points > highscore:
-                highscore=points 
-            text = font.render("High Score: "+ str(highscore) + "  Points: " + str(points), True, FONT_COLOR)
+    def score_l1():
+        global points_l1, game_speed, distance_l1
+        distance_l1 += 1
+        if distance_l1 % 100 == 0:
+            game_speed += 1
+        # with open("score.txt", "r") as f:
+        #     score_ints = [int(x) for x in f.read().split()]  
+        #     highscore = max(score_ints)
+        #     if points_l1 > highscore:
+        #         highscore=points_l1 
+        #     text = font.render("High Score: "+ str(highscore) + "  Points: " + str(points_l1), True, FONT_COLOR)
+        text = font.render("  Points: " + str(points_l1), True, FONT_COLOR)
         textRect = text.get_rect()
         textRect.center = (900, 40)
         SCREEN.blit(text, textRect)
@@ -527,10 +480,6 @@ def main():
         
         if random.randint(0, 200) == 5:  # Adjust the frequency as needed
             bomb_items.append(BombItem())
-        
-        if random.randint(0, 200) == 5:  # Adjust the frequency as needed
-            if game_speed>10:
-                slowmo_items.append(SlowMoItem())
 
         # Update and draw bullet items
         for item in bullet_items[:]:  # Use a slice copy to iterate safely when removing items
@@ -538,10 +487,6 @@ def main():
             item.draw(SCREEN)
         
         for item in bomb_items[:]:
-            item.update()
-            item.draw(SCREEN)
-        
-        for item in slowmo_items[:]:
             item.update()
             item.draw(SCREEN)
 
@@ -557,28 +502,6 @@ def main():
                 player.music_channel.play(player.sounds_list['bomb'])
                 obstacles.clear()
                 bomb_items.remove(item)  # Remove the collected item
-        
-        for item in slowmo_items[:]:  # Use a slice copy to iterate safely when removing items
-            if player.soldier_rect.colliderect(item.rect):
-                player.music_channel.play(player.sounds_list['slow'])
-                game_speed -= 2
-                if game_speed == 0 or game_speed<0:
-                    game_speed = 1
-                slowmo_items.remove(item)
-        
-        # for obstacle in obstacles:
-        #     obstacle.draw(SCREEN)
-        #     obstacle.update()
-        #     if player.hitbox.colliderect(obstacle.rect) and not player.soldier_dead:
-        #         death_count += 1
-        #         player.soldier_dead = True  # Set the soldier as dead
-        #         player.dead()  # Call the dead method to handle the death event
-        #         break 
-        #     for fireball in fireballs:
-        #         if fireball.collides_with(obstacle):  # You'll need to implement this method
-        #             obstacles.remove(obstacle)
-        #             fireballs.remove(fireball)
-        #             break
         
         fireballs = [fireball for fireball in fireballs if fireball.x < SCREEN_WIDTH]
 
@@ -610,7 +533,192 @@ def main():
                 menu(death_count)
             for fireball in fireballs:
                 if fireball.collides_with(obstacle):
-                    points += 50
+                    points_l1 += 50
+                    obstacles.remove(obstacle)
+                    fireballs.remove(fireball)
+                    break
+        
+        if points_l1 >= 100:
+            level_transition()
+        # background()
+
+        for fireball in fireballs:
+            fireball.move()
+            fireball.draw(SCREEN)
+        
+        for item in bomb_items[:]:
+            item.draw(SCREEN)
+        
+        for item in bullet_items[:]:  # Use a slice copy to iterate safely when removing items
+            item.draw(SCREEN)
+        
+        cloud.draw(SCREEN)
+        cloud.update()
+        display_bullet_count()
+
+        score_l1()
+
+        clock.tick(60)
+        pygame.display.update()
+
+def main_l2():
+    global fireballs, fireball_count, death_count, distance_l2
+    global game_speed, x_pos_bg, y_pos_bg, points_l2, obstacles
+    distance_l2 = 0
+    run = True
+    clock = pygame.time.Clock()
+    player = soldier()
+    cloud = Cloud()
+    game_speed = 10
+    x_pos_bg = 0
+    y_pos_bg = 380
+    points_l2 = 0
+    font = pygame.font.Font("freesansbold.ttf", 20)
+    obstacles = []
+    death_count = 0
+    pause = False
+    fireball_count = 3
+
+    pygame.key.set_repeat(50, 50)
+
+    def display_bullet_count():
+        font = pygame.font.Font("freesansbold.ttf", 20)
+        text = font.render(f'Bullets: {fireball_count}', True, FONT_COLOR)
+        text_rect = text.get_rect(topleft=(10, 10))  # Position at top-left corner
+        SCREEN.blit(text, text_rect)
+
+    def score_l2():
+        global points_l2, game_speed, distance_l2
+        distance_l2 += 1
+        if distance_l2 % 100 == 0:
+            game_speed += 1
+        # with open("score.txt", "r") as f:
+        #     score_ints = [int(x) for x in f.read().split()]  
+        #     highscore = max(score_ints)
+        #     if points_l1 > highscore:
+        #         highscore=points_l1 
+        #     text = font.render("High Score: "+ str(highscore) + "  Points: " + str(points_l1), True, FONT_COLOR)
+        text = font.render("  Points: " + str(points_l2), True, FONT_COLOR)
+        textRect = text.get_rect()
+        textRect.center = (900, 40)
+        SCREEN.blit(text, textRect)
+
+    # def background():
+    #     global x_pos_bg, y_pos_bg
+    #     image_width = BG.get_width()
+    #     SCREEN.blit(BG, (x_pos_bg, y_pos_bg))
+    #     SCREEN.blit(BG, (image_width + x_pos_bg, y_pos_bg))
+    #     if x_pos_bg <= -image_width:
+    #         SCREEN.blit(BG, (image_width + x_pos_bg, y_pos_bg))
+    #         x_pos_bg = 0
+    #     x_pos_bg -= game_speed
+
+    def unpause():
+        nonlocal pause, run
+        pause = False
+        run = True
+
+    def paused():
+        nonlocal pause
+        pause = True
+        font = pygame.font.Font("freesansbold.ttf", 30)
+        text = font.render("Game Paused, Press 'u' to Unpause", True, FONT_COLOR)
+        textRect = text.get_rect()
+        textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT  // 3)
+        SCREEN.blit(text, textRect)
+        pygame.display.update()
+
+        while pause:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_u:
+                    unpause()
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.display.quit()
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    run = False
+                    paused()
+                if event.key == pygame.K_SPACE and fireball_count > 0:
+                    player.shoot()
+                    fireballs.append(Fireball(player.soldier_rect.x, player.soldier_rect.y))
+                    fireball_count -= 1
+                if event.key == pygame.K_UP:
+                    print("up")
+                    player.soldier_rect.y = max(player.MIN_Y_POS, player.soldier_rect.y - player.VERTICAL_MOVE_SPEED)
+                if event.key == pygame.K_DOWN:
+                    print("down")
+                    player.soldier_rect.y = min(player.MAX_Y_POS, player.soldier_rect.y + player.VERTICAL_MOVE_SPEED)
+
+
+            # Add bullet items to the game
+        if random.randint(0, 60) == 5:  # Adjust the frequency as needed
+            bullet_items.append(BulletItem())
+        
+        if random.randint(0, 200) == 5:  # Adjust the frequency as needed
+            bomb_items.append(BombItem())
+
+        # Update and draw bullet items
+        for item in bullet_items[:]:  # Use a slice copy to iterate safely when removing items
+            item.update()
+            item.draw(SCREEN)
+        
+        for item in bomb_items[:]:
+            item.update()
+            item.draw(SCREEN)
+
+        # Check for collisions with bullet items
+        for item in bullet_items[:]:  # Use a slice copy to iterate safely when removing items
+            if player.soldier_rect.colliderect(item.rect):
+                player.music_channel.play(player.sounds_list['item_collect'])
+                fireball_count += 1  # Increase bullet count
+                bullet_items.remove(item)  # Remove the collected item
+        
+        for item in bomb_items[:]:  # Use a slice copy to iterate safely when removing items
+            if player.soldier_rect.colliderect(item.rect):
+                player.music_channel.play(player.sounds_list['bomb'])
+                obstacles.clear()
+                bomb_items.remove(item)  # Remove the collected item
+        
+        fireballs = [fireball for fireball in fireballs if fireball.x < SCREEN_WIDTH]
+
+        SCREEN.fill((255, 255, 255))
+        userInput = pygame.key.get_pressed()
+
+        # background()
+        back_ground.update()
+        back_ground.render()
+
+        player.draw(SCREEN)
+        player.update(userInput)
+
+        if len(obstacles) == 0:
+            if random.randint(0, 2) == 0:
+                obstacles.append(SmallCactus(SMALL_CACTUS))
+            elif random.randint(0, 2) == 1:
+                obstacles.append(LargeCactus(LARGE_CACTUS))
+            elif random.randint(0, 2) == 2:
+                obstacles.append(Bird(BIRD))
+
+        for obstacle in obstacles:
+            obstacle.draw(SCREEN)
+            obstacle.update()
+            if player.hitbox.colliderect(obstacle.rect):
+                death_count+=1
+                player.dead()
+                pygame.time.delay(1000)
+                menu(death_count)
+            for fireball in fireballs:
+                if fireball.collides_with(obstacle):
+                    points_l2 += 50
                     obstacles.remove(obstacle)
                     fireballs.remove(fireball)
                     break
@@ -627,21 +735,18 @@ def main():
         for item in bullet_items[:]:  # Use a slice copy to iterate safely when removing items
             item.draw(SCREEN)
         
-        for item in slowmo_items[:]:  # Use a slice copy to iterate safely when removing items
-            item.draw(SCREEN)
-        
         cloud.draw(SCREEN)
         cloud.update()
         display_bullet_count()
 
-        score()
+        score_l2()
 
         clock.tick(60)
         pygame.display.update()
 
 
 def menu(death_count):
-    global points
+    global points_l1, points_l2
     global FONT_COLOR
     run = True
     while run:
@@ -652,13 +757,13 @@ def menu(death_count):
         if death_count == 0:
             text = font.render("Protector Of World, Press enter to play", True, FONT_COLOR)
         elif death_count > 0:
-            text = font.render("Press any Key to Restart", True, FONT_COLOR)
-            score = font.render("Your Score: " + str(points), True, FONT_COLOR)
+            text = font.render("World have been invaded!", True, FONT_COLOR)
+            score = font.render("Your Score: " + str(points_l1), True, FONT_COLOR)
             scoreRect = score.get_rect()
             scoreRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50)
             SCREEN.blit(score, scoreRect)
             f = open("score.txt", "a")
-            f.write(str(points) + "\n")
+            f.write(str(points_l1) + "\n")
             f.close()
             with open("score.txt", "r") as f:
                 score = (
@@ -689,8 +794,52 @@ def menu(death_count):
             if event.type == pygame.KEYDOWN and death_count == 0:
                 dialogue()
             if event.type == pygame.KEYDOWN and death_count >= 1:
-                main()
-            
+                main_l1()
+
+def level_transition():
+    global FONT_COLOR
+    run = True
+    while run:
+        FONT_COLOR=(255,255,255)
+        SCREEN.fill((0, 0, 0))
+        font = pygame.font.Font("freesansbold.ttf", 18)
+        SCREEN.blit(font.render("You defeated the first wave of the enemies.", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 100, SCREEN_HEIGHT // 2 - 60))
+        SCREEN.blit(font.render("The next and final wave will be brutal and hard to defeat", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 100, SCREEN_HEIGHT // 2 - 30))
+        SCREEN.blit(font.render("All hope lies with you.", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 100, SCREEN_HEIGHT // 2))
+        SCREEN.blit(font.render("Defeat them and save the world. Collect 2500 points to save the world", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 100, SCREEN_HEIGHT // 2 + 30))
+        SCREEN.blit(font.render("Press Any Key to Continue", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 100, SCREEN_HEIGHT // 2 + 60))
+        
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.display.quit()
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                main_l2()
+
+def end_dialogue():
+    global FONT_COLOR
+    run = True
+    while run:
+        FONT_COLOR=(255,255,255)
+        SCREEN.fill((0, 0, 0))
+        font = pygame.font.Font("freesansbold.ttf", 18)
+        SCREEN.blit(font.render("Congratulations!!! You defeated the enemies.", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 100, SCREEN_HEIGHT // 2 - 60))
+        SCREEN.blit(font.render("The world is save for now", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 100, SCREEN_HEIGHT // 2 - 30))
+        SCREEN.blit(font.render("We will prepare the next threat.", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 100, SCREEN_HEIGHT // 2))
+        SCREEN.blit(font.render("Press Any Key to Continue", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 100, SCREEN_HEIGHT // 2 + 60))
+        
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.display.quit()
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                menu(death_count=0)
 
 def dialogue():
     global FONT_COLOR
@@ -707,11 +856,11 @@ def dialogue():
         # skip = font.render("""Press Enter to Skip""", True, FONT_COLOR)
         # textRect = text.get_rect()
         # textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-        SCREEN.blit(font.render("The World is getting invaded by alien.", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 50, SCREEN_HEIGHT // 2 - 60))
-        SCREEN.blit(font.render("You are the only one to defeat them. Please help us protect the World!", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 50, SCREEN_HEIGHT // 2 - 30))
-        SCREEN.blit(font.render("Be careful of meteor, comet and aliens don't get touch by them.", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 50, SCREEN_HEIGHT // 2))
-        SCREEN.blit(font.render("You can shoot them if they come close.", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 50, SCREEN_HEIGHT // 2 + 30))
-        SCREEN.blit(font.render("Press Any Key to Continue", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 50, SCREEN_HEIGHT // 2 + 60))
+        SCREEN.blit(font.render("The World is getting invaded by alien.", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 100, SCREEN_HEIGHT // 2 - 60))
+        SCREEN.blit(font.render("You are the only one to defeat them. Please help us protect the World!", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 100, SCREEN_HEIGHT // 2 - 30))
+        SCREEN.blit(font.render("Be careful of meteor, comet and aliens don't get touch by them.", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 100, SCREEN_HEIGHT // 2))
+        SCREEN.blit(font.render("You can shoot them if they come close. Collect 1000 points to proceed to next level", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 100, SCREEN_HEIGHT // 2 + 30))
+        SCREEN.blit(font.render("Press Any Key to Continue", True, FONT_COLOR), (SCREEN_WIDTH // 3 - 100, SCREEN_HEIGHT // 2 + 60))
         
         pygame.display.update()
         for event in pygame.event.get():
@@ -722,7 +871,7 @@ def dialogue():
                 exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
-                    main()
+                    main_l1()
 
 
 
